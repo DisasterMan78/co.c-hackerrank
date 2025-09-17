e581915c03d46087a403574d9d80786606159c8e
David Benson <dmbenson1978@gmail.com>
2025-09-17 16:28:05 +0100

    Reinstate `react-query`;

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { I18N } from "./constants/i18n";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PaymentsPage } from "./components/PaymentsPage";
 
 // This is required for tests to pass if ReactQuery is used
 // you don't have to use this library in your solution.
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       // disable retries is required for tests to pass if ReactQuery is used
-//       retry: false,
-//     },
-//   },
-// });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // disable retries is required for tests to pass if ReactQuery is used
+      retry: false,
+    },
+  },
+});
 
 function App() {
 
   return (
-    // <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <div className="min-h-screen bg-gray-50">
         <header className="bg-white shadow-sm">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -28,7 +28,7 @@ function App() {
           <PaymentsPage />
         </main>
       </div>
-    // </QueryClientProvider>
+    </QueryClientProvider>
   );
 }
 
